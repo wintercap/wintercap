7be3854f5240c345960a12d8b27d27061be3c219
David Wintercap <david.bonnet.14@gmail.com>
2014-02-28 12:42:05 +0100

    Layout and design of the site

--- website/views.py
+++ website/views.py
@@ -1,7 +1,38 @@
+"""
+wintercap.fr website views
+"""
+
+from collections import OrderedDict
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def make_nav(active_page):
+    """
+    This function builds the navbar as an orderd dict.
+    Each page name is a key, its value is the destination view.
+    """
+    pages = (
+        'Accueil',
+        'Compétences',
+        'Réalisations',
+        'Contact',
+    )
+    views = (
+        'website.views.home',
+        'website.views.home',
+        'website.views.home',
+        'website.views.home',
+    )
+    nav_pages = OrderedDict(zip(pages, views))
+    return nav_pages
+
 
 def home(request):
+    """
+    Home page.
+    """
     page_title = 'Accueil'
+    nav_pages = make_nav(page_title)
+
     return render(request, 'website/home.html', locals())
